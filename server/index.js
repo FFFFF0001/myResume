c3d937cf2e85681eef80fe792cec7b75954e25af
coderxuan <mifind@sina.com>
2017-08-21 18:12:29 +0800

    delete braches

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req, res) => {
-  res.send('Hello , this is server home . \n\r May be , you shoule open the resume page.')
-})
-
-module.exports = router;
